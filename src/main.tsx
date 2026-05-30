3a5cff3c959c1dcabeb65e687d93c081c7af805d
pandorapanchan34-oss <pandorapanchan34@gmail.com>
2026-05-30 12:18:54 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@
  */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.ts'
+import App from './App.tsx'
 import './styles/globals.css' // 🛡️ スタイル層を完全結合！
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
