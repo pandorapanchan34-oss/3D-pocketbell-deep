9449f791bddd394a7a6d7b18db2c4da572833fd5
pandorapanchan34-oss <pandorapanchan34@gmail.com>
2026-05-30 11:29:08 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import autoprefixer from 'autoprefixer'
 // https://vite.dev/config/
 export default defineConfig({
   // 🪐 補正殻：GitHub PagesのURL（リポジトリ名）にベースパスを完全吸着させる
-  base: process.env.NODE_ENV === 'production' ? '/3D-pocketbell/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/3D-pocketbell-deep/' : '/',
   plugins: [react()],
   css: {
     postcss: {
